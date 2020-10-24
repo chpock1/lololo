16e72d0fb2ed3fd24b356b02bc40de8aec0a2fac
ilya <rollerilya@gmail.com>
2020-10-24 19:07:53 +0300

    определение цветов

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
@@ -16,13 +15,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(function (req, res, next) {
+
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, XMLHttpRequest");
+  next();
+});
+
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
+
+
 
 // error handler
 app.use(function(err, req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,73 @@
 var express = require('express');
 var router = express.Router();
-
-
+multiparty = require('multiparty');
+var imageColors = require('imagecolors');
+const fs = require('fs');
+const colors=''
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+router.post('/colorsImg', function(req, res){
+  // создаем форму
+  var form = new multiparty.Form();
+  var uploadFile = {uploadPath: '', type: '', size: 0};
+  var maxSize = 100 * 1024 * 1024;
+  var supportMimeTypes = ['image/jpg', 'image/jpeg', 'image/png'];
+  var errors = [];
+  var nameImg='';
+  //если произошла ошибка
+  form.on('error', function(err){
+    if(fs.existsSync(uploadFile.path)) {
+      fs.unlinkSync(uploadFile.path);
+      console.log('error');
+    }
+  });
+  form.on('close', function() {
+    if(errors.length == 0) {
+      res.send({status: 'ok', text: 'Success',name:nameImg});
+    }
+    else {
+      if(fs.existsSync(uploadFile.path)) {
+        fs.unlinkSync(uploadFile.path);
+      }
+      res.send({status: 'bad', errors: errors});
+    }
+  });
+  form.on('part', function(part) {
+    uploadFile.size = part.byteCount;
+    uploadFile.type = part.headers['content-type']
+    uploadFile.path = './' + part.filename;
+    nameImg=part.filename;
+    console.log(part)
+    if(uploadFile.size > maxSize) {
+      errors.push('File size is ' + uploadFile.size + '. Limit is' + (maxSize / 1024 / 1024) + 'MB.');
+    }
+    if(supportMimeTypes.indexOf(uploadFile.type) == -1) {
+      errors.push('Unsupported mimetype ' + uploadFile.type);
+    }
+    if(errors.length == 0) {
+      var out = fs.createWriteStream(uploadFile.path);
+      part.pipe(out);
+    }
+    else {
+      part.resume();
+    }
+  });
+
+  form.parse(req);
+
+
+});
+router.post('/colors', function(req, res){
+
+  imageColors.extract('./'+req.body.name, 6, function (err, colors) {
+    this.colors=colors
+  });
+  res.send(this.colors)
 });
 
 module.exports = router;
